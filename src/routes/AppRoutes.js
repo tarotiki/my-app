3ed1c0a3801e2f0e2f1fd7834d8949298b29e34d
Keitaro Kusano <taroyamaguchi0106@gmail.com>
2024-12-09 18:38:07 +0900

    lineAPIfunctionwasdeployed

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import routes from "../routes";
 
-function AppRoutes() {
+function AppRoutes({ userProfile }) {
   return (
     <Routes>
       {routes.map((route, index) => (
-        <Route key={index} path={route.path} element={route.element} />
+        <Route
+          key={index}
+          path={route.path}
+          element={React.cloneElement(route.element, { userProfile })}
+        />
       ))}
     </Routes>
   );
