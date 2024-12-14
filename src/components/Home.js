c17e106eb3a5eff511176a8347401fceae78c871
Keitaro Kusano <taroyamaguchi0106@gmail.com>
2024-12-15 08:22:57 +0900

    ログアウトなし設計

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,10 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="home-container">
-      <h1>薬局サービスへようこそ</h1>
+      <h1>オーライLINE</h1>
       <div className="menu">
         <Link to="/upload" className="menu-link">
           処方箋をアップロード
-        </Link>
-        <Link to="/appointments" className="menu-link">
-          予約一覧を見る
-        </Link>
-        <Link to="/notifications" className="menu-link">
-          通知を見る
         </Link>
         <Link to="/select-delivery" className="menu-link">
           来店・配送の予約
